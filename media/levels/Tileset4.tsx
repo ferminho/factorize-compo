38fcc90a2f3000fd1626de49bb0173542880e905
Fermin Vicente <ferminho@gmail.com>
2021-03-03 23:08:30 +0100

    Basic tests with Tiled

---
+++ media/levels/Tileset4.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="Tileset4" tilewidth="4" tileheight="4" tilecount="64" columns="64">
+ <image source="../../src/factorize.data/gfx/tileset4.png" width="256" height="4"/>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
